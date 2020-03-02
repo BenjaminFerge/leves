3b94a3aa1f654cbc9d8e3588ad0fca100bfa60f4
Benjámin J. Ferge <benjamin.ferge@protonmail.com>
2020-03-02 17:37:22 +0100

    Replace vcpkg with Conan

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,7 @@
 rm -rf build
 mkdir build
 pushd build
+conan install ..
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE="iwyu;-Xiwyu;any;-Xiwyu;iwyu;-Xiwyu;args" ..
 cmake --build .
 popd
